c9343029e0d0530fe3175a7230b433727d4a0a3f
Salman Mujtaba <salmanmujtaba@macbook.local>
2023-01-11 00:46:45 -0500

    Add Client config. Add Google sign in button.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import { Home } from './container/Home'
-import { Login } from './components/Login'
-import React from 'react'
+import { Home } from "./container/Home";
+import Login from "./components/Login";
+import React from "react";
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="login" element={<Login/>}></Route>
-      <Route path="/*" element={<Home/>}></Route>
+      <Route
+        path='login'
+        element={<Login />}></Route>
+      <Route
+        path='/*'
+        element={<Home />}></Route>
     </Routes>
-  )
-}
+  );
+};
