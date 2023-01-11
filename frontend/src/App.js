104cec83149e554a9452b741b86fd0bcff92819d
Salman Mujtaba <salmanmujtaba@macbook.local>
2023-01-10 22:40:59 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { Home } from './container/Home'
+import { Login } from './components/Login'
+import React from 'react'
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="login" element={<Login/>}></Route>
+      <Route path="/*" element={<Home/>}></Route>
+    </Routes>
+  )
+}
