0a854dccc1243df57c5563eb001129f71a412c7c
Salman Mujtaba <salmanmujtaba@macbook.local>
2023-01-14 03:44:07 -0500

    WIP add sidebar and other components

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+import { HiMenu } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import { client } from "../client";
+import logo from "../assets/logo.png";
+import { userQuery } from "../utils/data";
 
 export const Home = () => {
+  const [user, setUser] = useState(null);
+  const userInfo =
+    !!localStorage.getItem("user") && JSON.parse(localStorage.getItem("user"));
+  const [toggleSideBar, setToggleSideBar] = useState(false);
+  useEffect(() => {
+    const query = userQuery(userInfo?._id);
+    client.fetch(query).then((data) => {
+      console.log(data[0]);
+      setUser(data[0]);
+    });
+    return () => {
+      // cleanup;
+    };
+  }, []);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-100 ease-out '>
+      <div className='hidden md:flex h-screen flex-initial'>
+        <Sidebar></Sidebar>
+      </div>
+      <div className='flex md:hidden flex-row'>
+        <HiMenu
+          fontSize={40}
+          className='cursor-pointer'
+          onClick={() => setToggleSideBar(false)}></HiMenu>
+        <Link to='/'>
+          <img
+            className='w-28'
+            src={logo}
+            alt='logo'></img>
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          {" "}
+          <img
+            className='w-28'
+            src={user?.image}
+            alt='user'></img>
+        </Link>
+      </div>
+    </div>
+  );
+};
